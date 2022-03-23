9e2686a0d1e47c49905577fccf38eedee9083342
codfish2140 <pony2140@livemail.tw>
2022-03-24 00:25:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+  <q-btn icon="home">安安</q-btn>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+</script>
+
+<style lang="sass">
+#app
+  display: flex
+  flex-direction: column
+</style>
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/zh-TW'
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+import App from './App.vue'
+
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+  lang: quasarLang,
+})
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app')
